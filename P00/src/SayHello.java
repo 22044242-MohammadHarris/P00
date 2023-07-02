52aebff5a6478b2eac731090a388e112255a85c8
22044242_harris <22044242@myrp.edu.sg>
2023-07-02 14:57:41 +0800

    Initial commit

---
+++ P00/src/SayHello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author 22044242
+ *
+ */
+public class SayHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello there");
+	}
+
+}
